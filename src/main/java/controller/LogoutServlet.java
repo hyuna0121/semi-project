bb38f9c579cd53d93bdb6eda1b9f348fb736ff68
hyunsungSm <oohs942230@naver.com>
2025-11-03 10:23:52 +0900

    로그인 로그아웃 수정

---
+++ src/main/java/controller/LogoutServlet.java
@@ -0,0 +1,40 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/Logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	    if (req.getSession(false) != null) req.getSession(false).invalidate();
+	    resp.sendRedirect(req.getContextPath() + "/mainpage/mainpage.jsp");
+	}
+
+}
--- src/main/webapp/login/loginProc.jsp
+++ src/main/webapp/login/loginProc.jsp
@@ -41,7 +41,7 @@
 	                session.setAttribute("loginId", id);
 	                session.setAttribute("loginName", name);
 	                // 메인으로 이동
-	                response.sendRedirect("loginsuccess.jsp");
+	                response.sendRedirect("../mainpage/mainpage.jsp");
 	                return;
 	            } else {
 	                out.println("<script>alert('로그인 실패: 아이디 또는 비밀번호가 올바르지 않습니다.'); location.href='" + ctx + "/login/login.jsp';</script>");
